e7fd23ae7d11aadb77aa09142d7e427b2b82d1cc
saidions01 <101123304+saidions01@users.noreply.github.com>
2025-07-02 16:21:23 +0100

    Emproved the search bar visibility

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,127 +1,140 @@
 sap.ui.define(
-	["./BaseController", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"],
-	function (BaseController, MessageToast, JSONModel) {
-		"use strict";
+  ["./BaseController", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"],
+  function (BaseController, MessageToast, JSONModel) {
+    "use strict";
 
-		return BaseController.extend("com.ridesharing.controller.App", {
-			onInit: function () {
-				
-				let storedData = localStorage.getItem("rideData");
+    return BaseController.extend("com.ridesharing.controller.App", {
+      onInit: function () {
+        let storedData = localStorage.getItem("rideData");
 
-				if (!storedData) {
-					// Default data
-					storedData = {
-						rides: [],
-						newRide: {
-							rideId: "",
-							driverName: "",
-							vehicle: "",
-							availableSeats: "",
-							pickupLocation: "",
-							destination: "",
-						},
-					};
-					localStorage.setItem("rideData", JSON.stringify(storedData));
-				} else {
-					storedData = JSON.parse(storedData);
-				}
+        if (!storedData) {
+          storedData = {
+            rides: [],
+            newRide: {
+              rideId: "",
+              driverName: "",
+              vehicle: "",
+              availableSeats: "",
+              pickupLocation: "",
+              destination: "",
+            },
+            showSearchResults: false 
+          };
+          localStorage.setItem("rideData", JSON.stringify(storedData));
+        } else {
+          storedData = JSON.parse(storedData);
+          // Make sure the property exists after loading
+          if (typeof storedData.showSearchResults === "undefined") {
+            storedData.showSearchResults = false;
+          }
+        }
 
-				const oModel = new JSONModel(storedData);
-				this.getView().setModel(oModel);
-			},
-			onAddRide: function () {
-				const oModel = this.getView().getModel();
-				const oNewRide = oModel.getProperty("/newRide");
-				const aRides = oModel.getProperty("/rides") || [];
+        const oModel = new JSONModel(storedData);
+        this.getView().setModel(oModel);
+      },
 
-				// 🔢 Auto-increment rideId
-				const maxId = aRides.reduce(
-					(max, ride) => Math.max(max, parseInt(ride.rideId, 10)),
-					0
-				);
-				const newId = maxId + 1;
+      onAddRide: function () {
+        const oModel = this.getView().getModel();
+        const oNewRide = oModel.getProperty("/newRide");
+        const aRides = oModel.getProperty("/rides") || [];
 
-				// Validation
-				if (
-					!oNewRide.driverName.trim() ||
-					!oNewRide.vehicle.trim() ||
-					!oNewRide.availableSeats ||
-					!oNewRide.pickupLocation.trim() ||
-					!oNewRide.destination.trim()
-				) {
-					MessageToast.show("Please fill all fields");
-					return;
-				}
+        const maxId = aRides.reduce(
+          (max, ride) => Math.max(max, parseInt(ride.rideId, 10)),
+          0
+        );
+        const newId = maxId + 1;
 
-				const seats = Number(oNewRide.availableSeats);
-				if (isNaN(seats) || seats <= 0) {
-					MessageToast.show("Available seats must be a positive number");
-					return;
-				}
+        if (
+          !oNewRide.driverName.trim() ||
+          !oNewRide.vehicle.trim() ||
+          !oNewRide.availableSeats ||
+          !oNewRide.pickupLocation.trim() ||
+          !oNewRide.destination.trim()
+        ) {
+          MessageToast.show("Please fill all fields");
+          return;
+        }
 
-				// Push the new ride with the auto-generated ID
-				aRides.push({
-					rideId: newId,
-					driverName: oNewRide.driverName,
-					vehicle: oNewRide.vehicle,
-					availableSeats: seats,
-					pickupLocation: oNewRide.pickupLocation,
-					destination: oNewRide.destination,
-				});
+        const seats = Number(oNewRide.availableSeats);
+        if (isNaN(seats) || seats <= 0) {
+          MessageToast.show("Available seats must be a positive number");
+          return;
+        }
 
-				oModel.setProperty("/rides", aRides);
+        aRides.push({
+          rideId: newId,
+          driverName: oNewRide.driverName,
+          vehicle: oNewRide.vehicle,
+          availableSeats: seats,
+          pickupLocation: oNewRide.pickupLocation,
+          destination: oNewRide.destination,
+        });
 
-				// Clear the form
-				oModel.setProperty("/newRide", {
-					rideId: "",
-					driverName: "",
-					vehicle: "",
-					availableSeats: "",
-					pickupLocation: "",
-					destination: "",
-				});
+        oModel.setProperty("/rides", aRides);
 
-				// Persist to localStorage
-				localStorage.setItem("rideData", JSON.stringify(oModel.getData()));
+        oModel.setProperty("/newRide", {
+          rideId: "",
+          driverName: "",
+          vehicle: "",
+          availableSeats: "",
+          pickupLocation: "",
+          destination: "",
+        });
 
-				MessageToast.show("Ride added successfully");
-			},
-			onDeleteRide: function (oEvent) {
-				const oModel = this.getView().getModel();
-				const oItem = oEvent.getSource().getBindingContext().getObject();
-				const aRides = oModel.getProperty("/rides");
+        localStorage.setItem("rideData", JSON.stringify(oModel.getData()));
 
-				const iIndex = aRides.findIndex((ride) => ride.rideId === oItem.rideId);
-				if (iIndex !== -1) {
-					aRides.splice(iIndex, 1);
-					oModel.setProperty("/rides", aRides);
-					MessageToast.show("Ride deleted successfully");
-				}
-			},
+        MessageToast.show("Ride added successfully");
+      },
 
-			onSearch: function (oEvent) {
-				const sQuery = oEvent.getParameter("newValue").toLowerCase();
-				const oList = this.byId("rideList");
-				const oBinding = oList.getBinding("items");
+      onDeleteRide: function (oEvent) {
+        const oModel = this.getView().getModel();
+        const oItem = oEvent.getSource().getBindingContext().getObject();
+        const aRides = oModel.getProperty("/rides");
 
-				oBinding.filter([
-					new sap.ui.model.Filter({
-						filters: [
-							new sap.ui.model.Filter(
-								"driverName",
-								sap.ui.model.FilterOperator.Contains,
-								sQuery
-							),
-							new sap.ui.model.Filter(
-								"pickupLocation",
-								sap.ui.model.FilterOperator.Contains,
-								sQuery
-							),
-						],
-						and: false,
-					}),
-				]);
-			},
-		});
-	}
+        const iIndex = aRides.findIndex((ride) => ride.rideId === oItem.rideId);
+        if (iIndex !== -1) {
+          aRides.splice(iIndex, 1);
+          oModel.setProperty("/rides", aRides);
+          localStorage.setItem("rideData", JSON.stringify(oModel.getData()));
+          MessageToast.show("Ride deleted successfully");
+        }
+      },
+
+      onSearch: function (oEvent) {
+        const sQuery = oEvent.getParameter("newValue").toLowerCase();
+        const oModel = this.getView().getModel();
+
+        if (!sQuery || sQuery.length === 0) {
+          oModel.setProperty("/showSearchResults", false);
+
+          const oList = this.byId("rideList");
+          oList.getBinding("items").filter([]);
+          return;
+        }
+
+        oModel.setProperty("/showSearchResults", true);
+
+        const oList = this.byId("rideList");
+        const oBinding = oList.getBinding("items");
+
+        oBinding.filter([
+          new sap.ui.model.Filter({
+            filters: [
+              new sap.ui.model.Filter(
+                "driverName",
+                sap.ui.model.FilterOperator.Contains,
+                sQuery
+              ),
+              new sap.ui.model.Filter(
+                "pickupLocation",
+                sap.ui.model.FilterOperator.Contains,
+                sQuery
+              ),
+            ],
+            and: false,
+          }),
+        ]);
+      },
+    });
+  }
 );
